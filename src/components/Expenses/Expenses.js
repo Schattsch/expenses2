1d83f52fd0b88dcd80a5e442dc3fc02a22fb5f68
Schattsch <karelsch123@gmail.com>
2023-02-01 15:17:38 +0200

    Andmete kuvamine sõltuvalt tingimusest

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,19 +16,27 @@ function Expenses(props) {
         setFilteredYear(year)
     }
 
+    const filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear
+    })
+
+    let expensesContent = <p>No expenses found.</p>
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return <ExpenseItem
+                id={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+                ></ExpenseItem>
+        })
+    }
+
+
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-            {
-                props.expenses.filter(expense => expense.date.getFullYear().toString() === filteredYear).map((expense) => {
-                    return <ExpenseItem
-                        id={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    ></ExpenseItem>
-                })
-            }
+            {expensesContent}
         </Card>
     )
 }
