e552a2c94850e0bd616434f6f9592331c30566bf
Schattsch <karelsch123@gmail.com>
2023-02-01 14:42:58 +0200

    Ülesanne 3 - Andmete suunamine (bottom-up) ja väärtuse hetkeseisu lugemine ning määramine + Andmete kuvamine nimekirjast

--- src/App.js
+++ src/App.js
@@ -3,20 +3,19 @@ import './App.css';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-    const expenses = [
+const DUMMY_EXPENSES = [
     {
-     date: new Date(2023,0,10),
-     title: 'New book',
-     price: 30.99
+        date: new Date(2023,0,10),
+        title: 'New book',
+        price: 30.99
     },
     {
-     date: new Date(2023,0,10),
-     title: 'New jeans',
-     price: 99.99
+        date: new Date(2023,0,10),
+        title: 'New jeans',
+        price: 99.99
     }
 ]
-
+const App = () => {
     const addExpensehandler = (expense) => {
         console.log('In App.js')
         console.log(expense)
@@ -25,7 +24,7 @@ const App = () => {
     return (
     <div className="App">
         <NewExpense onAddExpense={addExpensehandler}></NewExpense>
-        <Expenses expenses={expenses}></Expenses>
+        <Expenses expenses={DUMMY_EXPENSES}></Expenses>
     </div>
   );
 }
