fc67fb81630d9adfce5c2db64d3d937d72f5617a
Schattsch <karelsch123@gmail.com>
2023-02-01 13:52:38 +0200

    Loetud sisendi kasutamine REVERT

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,34 +1,22 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css'
 const ExpenseForm = () => {
-    //const [enteredTitle, setEnteredTitle] = useState('')
-    //const [enteredAmount, setEnteredAmount] = useState('')
-    //const [enteredDate, setEnteredDate] = useState('')
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
-    console.log(userInput)
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
 
     const titleChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        })
-    }
+            setEnteredTitle(event.target.value)
+        }
+
     const amountChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        })
-    }
+            setEnteredAmount(event.target.value)
+        }
+
     const dateChangeHandler = (event) => {
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        })
-    }
+            setEnteredDate(event.target.value)
+        }
+
 
 return (
         <form>
