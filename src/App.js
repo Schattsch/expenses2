d4938a7839ebc0a7fec52736795e11e3ec9af261
Schattsch <karelsch123@gmail.com>
2023-02-01 11:33:28 +0200

    Uue komponendi loomine ja kujundamine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
