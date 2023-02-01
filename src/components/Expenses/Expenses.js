1cbda781db6355b2de220ac881e6190979870b6c
Schattsch <karelsch123@gmail.com>
2023-02-01 15:39:55 +0200

    Eraldi komponendi abil

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import './Expenses.css'
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter"
+import ExpensesList from "./ExpensesList";
 
 
 
@@ -20,23 +20,13 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear
     })
 
-    let expensesContent = <p>No expenses found.</p>
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => {
-            return <ExpenseItem
-                id={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-                ></ExpenseItem>
-        })
-    }
+
 
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
-            {expensesContent}
+            <ExpensesList filteredExpenses={filteredExpenses}></ExpensesList>
         </Card>
     )
 }
