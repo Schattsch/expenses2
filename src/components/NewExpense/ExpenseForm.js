dc61dfac0ff295aef9701f93117ddc3a95b0f330
Schattsch <karelsch123@gmail.com>
2023-02-01 13:29:51 +0200

    Loetud sisendi kasutamine

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css'
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+
+
     const titleChangeHandler = (event) => {
-    console.log(event.target.value)
-}
+        setEnteredTitle(event.target.value)
+    }
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+    }
 
 return (
         <form>
@@ -15,11 +26,12 @@ return (
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01"/>
+                    <input type="number" min="1" step="1" onChange={amountChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2023-01-18" max="2025-12-31"/>
+                    <input type="date" min="2023-01-18" max="2025-12-31"
+                    onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
