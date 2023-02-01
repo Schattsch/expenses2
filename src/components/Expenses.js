4d4e1a2611556ded2a015563ad42a42883c82abd
Schattsch <karelsch123@gmail.com>
2023-02-01 12:30:33 +0200

    Abikomponentide loomine

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,12 +1,13 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem expenseData={props.expenses[0]}></ExpenseItem>
             <ExpenseItem expenseData={props.expenses[1]}></ExpenseItem>
-        </div>
+        </Card>
     )
 }
 
